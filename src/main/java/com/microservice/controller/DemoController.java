510e7d23ae18316525850be07ad399b31fa6688f
virajput <vijendra.rajput@gmail.com>
2016-01-02 20:42:06 +1100

    First commit

---
+++ src/main/java/com/microservice/controller/DemoController.java
@@ -0,0 +1,27 @@
+package com.microservice.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *@author Vijendra
+ *
+ */
+
+@RestController
+public class DemoController {
+	static Logger log = Logger.getLogger(DemoController.class);
+
+	public DemoController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		log.info("invoking rest service");
+		return "Hello Surfer!!!";
+	}
+}
+
+
